8490fac121e959baf9fac4b21e36f13c11b84e35
root <root@DESKTOP-7OS8CPN.localdomain>
2020-05-26 10:50:33 +0530

    YashKhanna_1527915

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,21 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
 	// Fill your code
-		
+		Connection c=null;
+		ConnectionManager cm=new ConnectionManager();
+		c=cm.getConnection();
+		if(c!=null) {
+			System.out.println("Connection Established");
+		}else {
+			System.out.println("Check your Connection");
+		}
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,22 @@
 package utility;
 
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
-	
-	
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		//load Driver Classs
+		Class.forName("oracle.jdbc.OracleDriver");
+		
+		//Establishing Connection
+		Connection con=null;
+		
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl","system","oracle");
+		
+		return con;
+	}
+
+
+
 }
